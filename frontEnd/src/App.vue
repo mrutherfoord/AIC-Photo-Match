a414f960d62575155ecf2f0d3fe466227e57b688
bradleyhop <bradleyhop@gmail.com>
2020-08-10 22:00:54 -0500

    props now show comuted colors

--- frontEnd/src/App.vue
+++ frontEnd/src/App.vue
@@ -30,6 +30,10 @@ export default {
 </template>
 
 <style lang="scss">
+body {
+  background-color: #f0f0f0;
+}
+
 #app {
   color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -41,7 +45,7 @@ export default {
 
 .app-copy {
   margin: auto;
-  width: 70%;
+  width: 50%;
 }
 
 .page-lay {
